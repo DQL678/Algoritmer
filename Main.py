0e25f563bb69c23ef3f7611dca41539e97e52433
ChristianProla <christian.prola@icloud.com>
2026-02-07 10:13:48 +0100

    Map.py er blevet omdøbbet tilhex_map.py og det køre nu ud fra main som har fået en startmenu

---
+++ Main.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+from hex_map import run_hex_map
+
+# Main vindue
+root = tk.Tk()
+root.title("Main Menu")
+root.geometry("300x250")
+
+# Overskrift
+label = tk.Label(root, text="Protect the flag", font=("Arial", 14))
+label.pack(pady=15)
+
+# Knap til map
+map_button = tk.Button(root, text="Åbn Hex Map", command=run_hex_map)
+map_button.pack(pady=10)
+
+root.mainloop()
